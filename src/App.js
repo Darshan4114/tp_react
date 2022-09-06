99dcda7e22e21beb26bc07c1df3e899c235ea3ea
Darshan Dhabale <darshan@darshandev.tech>
2022-09-06 23:34:07 +0530

    endpoint change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
   useEffect(() => {
     fetch(
-      "https://631779098b8f1c0461f15083--capable-dusk-4622f5.netlify.app/.netlify/functions/api/test"
+      "https://631779098b8f1c0461f15083--capable-dusk-4622f5.netlify.app/.netlify/functions/api/"
     )
       .then((res) => res.json())
       .then((res) => setTextData(res))
